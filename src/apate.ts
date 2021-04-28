1d2ff53a63442a86b196d6240cf96f26a6da6147
LonelyGriffn <lonelygriffin@gmail.com>
2021-04-28 10:45:47 +0300

    add general apate interface

--- src/apate.ts
+++ src/apate.ts
@@ -1,12 +1,17 @@
 import {DEFAULT_CONFIG, IConfig} from './config'
 
-export class Apate {
+export interface IApate {
+  run(): Promise<void>
+  shutdown(): Promise<void>
+}
+
+export class Apate implements IApate {
   constructor(config?: Partial<IConfig>) {
-    this.config = {
-      ...DEFAULT_CONFIG,
-      ...config
-    }
+    this.config = {...DEFAULT_CONFIG, ...config}
   }
+
+  async run() {}
+  async shutdown() {}
 
   private config: IConfig
 }
