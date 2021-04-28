3811f1bf31ff88592b605663ded3297ac4d52d23
LonelyGriffn <lonelygriffin@gmail.com>
2021-04-28 18:43:59 +0300

    remove mock protocol config from tests

--- test/apate-config.ts
+++ test/apate-config.ts
@@ -3,7 +3,6 @@ import {IApateConfig} from '../lib'
 export const TEST_APATE_CONFIG: IApateConfig = {
   controlHost: 'localhost',
   controlPort: 8100,
-  mockProtocol: 'http',
   mockHost: 'localhost',
   mockPort: 8000
 }
